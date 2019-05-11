f04f8203ae30cb65c577c9a9d9a3548529ee37b4
Faran <faranmushtaq16@gmail.com>
2019-05-11 16:23:06 +0500

    Initial Commit

---
+++ Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by Faran on 5/11/19.
+//  Copyright © 2019 Faran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
